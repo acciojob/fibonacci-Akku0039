1082f46fd3d7889f916ba430315245e69e5d59e4
Akku0039 <117562663+Akku0039@users.noreply.github.com>
2023-05-01 00:26:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 function fibonacci(num) {
 // your code here
+     if (num < 0) {
+    throw new Error("Input must be a non-negative integer.");
+  } else if (num <= 1) {
+    return num;
+  } else {
+    let a = 0;
+    let b = 1;
+    let c;
+    for (let i = 2; i <= num; i++) {
+      c = a + b;
+      a = b;
+      b = c;
+    }
+    return b;
+  
 }
 
 module.exports = fibonacci;
